2076b65d841fc62f2675d018e01f828290090a27
KelseyJuraschka <11klj4@queensu.ca>
2015-03-25 21:05:54 -0700

    modified make res and end res.

--- private_User.php
+++ private_User.php
@@ -20,6 +20,81 @@
     // a username is user submitted content we must use htmlentities on it before displaying it to the user. 
 ?> 
 Hello <?php echo htmlentities($_SESSION['member']['Email'], ENT_QUOTES, 'UTF-8'); ?>, secret content!<br /> 
-<a href="memberlist.php">Memberlist</a><br /> 
 <a href="edit_account.php">Edit Account</a><br /> 
 <a href="logout.php"><button>Logout</button></a>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>My Dashboard</title>
+<!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+<script $("button").ready(function(){
+	
+-->
+</head>
+
+<body>
+	<h2>Ongoing Reservations</h2>
+	
+	<table>
+		<th>VIN</th>
+		<th>PickupTime</th>
+		<th>Expected Return Time</th>
+		<th>Location</th>
+		<th>End Reservation</th>
+<?php
+	$memNoRes = $db->query("Select MemberNo from member where Email = '".$_SESSION['member']['Email']."'");
+	foreach ($memNoRes as $memNo1){
+	$memberNo = $memNo1['MemberNo'];
+	}
+
+
+	
+
+try {
+		#$dbh = new PDO('mysql:host=localhost;dbname=KTCS', "root", "");
+		$resQuery = "SELECT * FROM reservations join Locations on reservations.PickupLocNo = Locations.LocNo WHERE MemberNo = $memberNo and PickupTime<=NOW() order by PickupTime";
+		$rows = $db->query($resQuery);
+		echo $resQuery;
+		#echo "SELECT * FROM reservations WHERE MemberNo = $memberNo and PickupTime<=NOW() order by PickupTime"
+		foreach($rows as $row) {
+		$resNo = $row['ResNo'];
+	    #echo "<tr><td>".$row[0]."</td><td>".$row[1]."</td><td>".$row[2]."</td><td>".$row[3]"</td></tr>";			 
+		echo "<tr><td>".$row['VIN']."</td><td>".$row['PickupTime']."</td><td>".$row['ReturnTime']."</td><td>".$row['LocName']."</td>
+		<td><a href='endRes.php?resNo=".$resNo."&amp;memNo=".$memberNo."' >End Reservation</a></td></tr>";
+		#<td><form  method='POST'><input type='submit' name='resNo' value = 'End Reservation' id='". $resNo ."' /></form></td></tr>";
+#action='endRes.php'
+		}
+    
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+if(isset($_POST['resNo'])){
+echo "<p>".$_POST['resNo']."</p>";
+echo $sql = "INSERT INTO History
+(ResNo, MemberNo, VIN, ResDate, PickUpTime, PickupLocNo)
+Select ResNo, MemberNo, VIN, ResDate, PickUpTime, PickupLocNo
+FROM Reservations 
+WHERE ResNo = '".$_POST['resNo']."' ";
+$query = $db->query($sql);
+if ($query == 1) { 
+    echo 'Reservation Has Been Deleted';
+} else { 
+    echo 'Deletion Failed';
+} 
+}
+?>
+		
+		
+	</table>
+	
+	
+	
+	
+	
+	</body>
+	</html>
